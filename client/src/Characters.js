6754d357f368df4b673c8843821e92e6d588ae94
Dion <dionagoes@gmail.com>
2019-06-28 15:31:18 +0700

    game layout

---
+++ client/src/Characters.js
@@ -0,0 +1,170 @@
+module.exports = {
+  tidus: {
+    attack: {
+      motion: "/tidus/attack.gif",
+      delay: 500,
+      sound: new Audio("/noctis/sfx/attack.wav")
+    },
+    idle: {
+      motion: "/tidus/idle.gif",
+      delay: 0
+    },
+    ulti: {
+      motion: "/tidus/ulti.gif",
+      delay: 7800
+    },
+    dead: {
+      motion: "/tidus/dead.gif",
+      delay: 2000
+    },
+    win: {
+      motion: "/tidus/win-before.gif",
+      delay: 1000
+    },
+    winafter: {
+      motion: "/tidus/win-after.gif",
+      delay: 0
+    },
+    sound: new Audio("/tidus-atk.wav")
+  },
+  twob: {
+    attack: {
+      motion: "/2B/attack.gif",
+      delay: 4000,
+      sound: new Audio("/noctis/sfx/attack.wav")
+    },
+    idle: {
+      motion: "/2B/idle.gif",
+      delay: 0
+    },
+    ulti: {
+      motion: "/2B/ulti.gif",
+      delay: 5000
+    },
+    dead: {
+      motion: "/tidus/dead.gif",
+      delay: 2000
+    },
+    win: {
+      motion: "/2B/win-before.gif",
+      delay: 1000
+    },
+    winafter: {
+      motion: "/2B/win-after.gif",
+      delay: 0
+    },
+    sound: new Audio("/tidus-atk.wav")
+  },
+  noctis: {
+    attack: {
+      motion: "/noctis/attack.gif",
+      delay: 3300,
+      sound: new Audio("/noctis/sfx/attack.wav")
+    },
+    idle: {
+      motion: "/noctis/idle.gif",
+      delay: 0
+    },
+    ulti: {
+      motion: "/noctis/ulti.gif",
+      delay: 8000
+    },
+    dead: {
+      motion: "/noctis/dead.gif",
+      delay: 2000
+    },
+    win: {
+      motion: "/noctis/win-before.gif",
+      delay: 5000
+    },
+    winafter: {
+      motion: "/noctis/win-after.gif",
+      delay: 0
+    },
+    sound: new Audio("/tidus-atk.wav")
+  },
+  sephiroth: {
+    attack: {
+      motion: "/sephiroth/attack.gif",
+      delay: 4000,
+      sound: new Audio("/noctis/sfx/attack.wav")
+    },
+    idle: {
+      motion: "/sephiroth/idle.gif",
+      delay: 0
+    },
+    ulti: {
+      motion: "/sephiroth/ulti.gif",
+      delay: 5000
+    },
+    dead: {
+      motion: "/sephiroth/dead.gif",
+      delay: 2000
+    },
+    win: {
+      motion: "/sephiroth/win-before.gif",
+      delay: 1000
+    },
+    winafter: {
+      motion: "/sephiroth/win-after.gif",
+      delay: 0
+    },
+    sound: new Audio("/tidus-atk.wav")
+  },
+  sora: {
+    attack: {
+      motion: "/sora/attack.gif",
+      delay: 4000,
+      sound: new Audio("/noctis/sfx/attack.wav")
+    },
+    idle: {
+      motion: "/sora/idle.gif",
+      delay: 0
+    },
+    ulti: {
+      motion: "/sora/ulti.gif",
+      delay: 3000
+    },
+    dead: {
+      motion: "/sora/dead.gif",
+      delay: 2000
+    },
+    win: {
+      motion: "/sora/win-before.gif",
+      delay: 3200
+    },
+    winafter: {
+      motion: "/sora/win-after.gif",
+      delay: 0
+    },
+    sound: new Audio("/tidus-atk.wav")
+  },
+  dio: {
+    attack: {
+      motion: "/dio/attack.gif",
+      delay: 5000,
+      sound: new Audio("/noctis/sfx/attack.wav")
+    },
+    idle: {
+      motion: "/dio/idle.gif",
+      delay: 0
+    },
+    ulti: {
+      motion: "/dio/ulti.gif",
+      delay: 10000
+    },
+    dead: {
+      motion: "/dio/dead.gif",
+      delay: 2000
+    },
+    win: {
+      motion: "/dio/win-before.gif",
+      delay: 1000
+    },
+    winafter: {
+      motion: "/dio/win-after.gif",
+      delay: 0
+    },
+    sound: new Audio("/tidus-atk.wav")
+  },
+}
--- client/src/views/Game.vue
+++ client/src/views/Game.vue
@@ -15,27 +15,19 @@
               :disabled="turn === 'player2'"
               class="button is-dark"
               @mouseleave="selectionSound.pause(); selectionSound.currentTime=2"
-              @click="player1.commands.push('forward','attack','backward'); selectionSound.play()"
+              @click="attack(); selectionSound.play()"
             >attack</a>
-            <a
-              :disabled="turn === 'player2'"
-              class="button is-dark"
-              @click="player1.commands.push('forward','ulti','backward')"
-            >ulti</a>
-            <a
-              :disabled="turn === 'player2'"
-              class="button is-dark"
-              @click="player1.commands.push('win')"
-            >win</a>
-            <a
-              :disabled="turn === 'player2'"
-              class="button is-dark"
-              @click="player1.commands.push('dead')"
-            >dead</a>
-            <a :disabled="turn === 'player2'" class="button is-light" @click="execute">execute</a>
+            <a :disabled="turn === 'player2'" class="button is-dark" @click="ulti()">ulti</a>
+            <a :disabled="turn === 'player2'" class="button is-dark" @click="win()">win</a>
+            <a :disabled="turn === 'player2'" class="button is-dark" @click="dead()">dead</a>
+            <!-- <a :disabled="turn === 'player2'" class="button is-light" @click="execute">execute</a> -->
             <select :disabled="turn === 'player2'" v-model="player1.character">
               <option value="twob">2B</option>
               <option value="tidus">Tidus</option>
+              <option value="sora">Sora</option>
+              <option value="dio">Dio</option>
+              <option value="sephiroth">Sephiroth</option>
+              <option value="noctis">Noctis</option>
             </select>
           </div>
         </div>
@@ -54,33 +46,21 @@
             <select v-model="player2.character" :disabled="turn === 'player1'">
               <option value="twob">2B</option>
               <option value="tidus">Tidus</option>
+              <option value="sora">Sora</option>
+              <option value="dio">Dio</option>
+              <option value="sephiroth">Sephiroth</option>
+              <option value="noctis">Noctis</option>
             </select>
-            <a class="button is-light" @click="execute" :disabled="turn === 'player1'">execute</a>
-            <a
-              :disabled="turn === 'player1'"
-              class="button is-dark"
-              @click="player2.commands.push('forward','attack','backward')"
-            >attack</a>
-            <a
-              :disabled="turn === 'player1'"
-              class="button is-dark"
-              @click="player2.commands.push('forward','ulti','backward')"
-            >ulti</a>
-            <a
-              class="button is-dark"
-              @click="player2.commands.push('win')"
-              :disabled="turn === 'player1'"
-            >win</a>
-            <a
-              class="button is-dark"
-              @click="player2.commands.push('dead')"
-              :disabled="turn === 'player1'"
-            >dead</a>
+            <!-- <a class="button is-light" @click="execute" :disabled="turn === 'player1'">execute</a> -->
+            <a :disabled="turn === 'player1'" class="button is-dark" @click="attack()">attack</a>
+            <a :disabled="turn === 'player1'" class="button is-dark" @click="ulti()">ulti</a>
+            <a class="button is-dark" @click="win()" :disabled="turn === 'player1'">win</a>
+            <a class="button is-dark" @click="dead()" :disabled="turn === 'player1'">dead</a>
             <section>
               <div class="box">
-                <!-- <ul>
+                <ul>
                   <li v-for="(command,i) in player2.commands" :key="i">{{command}}</li>
-                </ul>-->
+                </ul>
                 <progress class="progress" value="15" max="100">15%</progress>
               </div>
             </section>
@@ -91,7 +71,9 @@
   </div>
 </template>
 <script>
+import { tidus, twob, dio, noctis, sephiroth, sora } from "../Characters.js";
 export default {
+  name: "Game",
   data() {
     return {
       audio: new Audio("/09.mp3"),
@@ -99,76 +81,12 @@ export default {
       selectionSound: new Audio("/selectSound.mp3"),
       muda: new Audio("/muda.MP3"),
       nani: new Audio("/nani.mp3"),
-      tidus: {
-        attack: {
-          motion: "/tidus/attack.gif",
-          delay: 500
-        },
-        idle: {
-          motion: "/tidus/idle.gif",
-          delay: 0
-        },
-        ulti: {
-          motion: "/tidus/ulti.gif",
-          delay: 10000
-        },
-        dead: {
-          motion: "/tidus/dead.gif",
-          delay: 2000
-        },
-        win: {
-          motion: "/tidus/win-before.gif",
-          delay: 1000
-        },
-        winafter: {
-          motion: "/tidus/win-after.gif",
-          delay: 0
-        },
-        forward: {
-          motion: "/tidus/move.gif",
-          delay: 500
-        },
-        backward: {
-          motion: "/tidus/back.gif",
-          delay: 500
-        },
-        sound: new Audio("/tidus-atk.wav")
-      },
-      twob: {
-        attack: {
-          motion: "/2B/attack.gif",
-          delay: 4000
-        },
-        idle: {
-          motion: "/2B/idle.gif",
-          delay: 0
-        },
-        ulti: {
-          motion: "/2B/ulti.gif",
-          delay: 5000
-        },
-        dead: {
-          motion: "/tidus/dead.gif",
-          delay: 2000
-        },
-        win: {
-          motion: "/2B/win-before.gif",
-          delay: 1000
-        },
-        winafter: {
-          motion: "/2B/win-after.gif",
-          delay: 0
-        },
-        forward: {
-          motion: "/tidus/move.gif",
-          delay: 500
-        },
-        backward: {
-          motion: "/tidus/back.gif",
-          delay: 500
-        },
-        sound: new Audio("/tidus-atk.wav")
-      },
+      tidus,
+      twob,
+      dio,
+      noctis,
+      sephiroth,
+      sora,
       player1: {
         active: "",
         commands: [],
@@ -179,102 +97,66 @@ export default {
         commands: [],
         character: ""
       },
-      background: "/hacktiv.jpeg",
+      background: "/batbg.png",
       turn: "player1"
     };
   },
   methods: {
     attack() {
-      console.log("attack");
+      console.log("atk");
       this[this.turn].active = this[this[this.turn].character].attack.motion;
-      this[this[this.turn].character].sound.play();
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          this[this.turn].active = this[this[this.turn].character].idle.motion;
-          resolve("done");
-        }, this[this[this.turn].character].attack.delay);
-      });
-    },
-    forward() {
-      console.log("forward");
-      this[this.turn].active = this[this[this.turn].character].forward.motion;
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve("done");
-        }, this[this[this.turn].character].forward.delay);
-      });
-    },
-    backward() {
-      console.log("backward");
-      this[this.turn].active = this[this[this.turn].character].backward.motion;
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          this[this.turn].active = this[this[this.turn].character].idle.motion;
-          resolve("done");
-        }, this[this[this.turn].character].backward.delay);
-      });
+      this[this[this.turn].character].attack.sound.play();
+      setTimeout(() => {
+        this[this.turn].active = this[this[this.turn].character].idle.motion;
+        this.turn === "player1"
+          ? (this.turn = "player2")
+          : (this.turn = "player1");
+      }, this[this[this.turn].character].attack.delay);
     },
     ulti() {
-      // setTimeout(() => {
-      //   this.muda.play();
-      // }, 1000);
-      // this.nani.currentTime = 0.1;
       this.nani.play();
-      return new Promise(resolve => {
-        console.log("ulti");
-        setTimeout(() => {
-          this[this.turn].active = this[this[this.turn].character].ulti.motion;
-          this.muda.play();
-        }, 3000);
-        setTimeout(() => {
-          this[this.turn].active = this[this[this.turn].character].idle.motion;
-          resolve("done");
-        }, this[this[this.turn].character].ulti.delay);
-      });
+      this[this.turn].active = this[this[this.turn].character].ulti.motion;
+      setTimeout(() => {
+        this[this.turn].active = this[this[this.turn].character].idle.motion;
+        this.turn === "player1"
+          ? (this.turn = "player2")
+          : (this.turn = "player1");
+      }, this[this[this.turn].character].ulti.delay);
     },
     dead() {
-      return new Promise(resolve => {
-        this[this.turn].active = this[this[this.turn].character].dead.motion;
-        setTimeout(() => {
-          console.log("dead");
-          resolve("done");
-        }, this[this[this.turn].character].dead.delay);
-      });
+      this[this.turn].active = this[this[this.turn].character].dead.motion;
+      setTimeout(() => {
+        this.turn === "player1"
+          ? (this.turn = "player2")
+          : (this.turn = "player1");
+      }, this[this[this.turn].character].dead.delay);
     },
     win() {
       console.log("start win");
       this.audio.pause();
       this.winsound.play();
-      return new Promise(resolve => {
-        this[this.turn].active = this[this[this.turn].character].win.motion;
-        setTimeout(() => {
-          console.log("win");
-          this[this.turn].active = this[
-            this[this.turn].character
-          ].winafter.motion;
-          resolve("done");
-        }, this[this[this.turn].character].win.delay);
-      });
-    },
-    async execute() {
-      for (let x = 0; x < this[this.turn].commands.length; x++) {
-        await this[this[this.turn].commands[x]]();
-      }
-      this[this.turn].commands = [];
-      this.turn === "player1"
-        ? (this.turn = "player2")
-        : (this.turn = "player1");
+      this[this.turn].active = this[this[this.turn].character].win.motion;
+      setTimeout(() => {
+        console.log("win");
+        this[this.turn].active = this[
+          this[this.turn].character
+        ].winafter.motion;
+        this.turn === "player1"
+          ? (this.turn = "player2")
+          : (this.turn = "player1");
+      }, this[this[this.turn].character].win.delay);
     }
   },
   mounted() {
-    this[this.turn].character = "tidus";
-    this[this.turn].active = this[this[this.turn].character].idle.motion;
-    this.player2.character = "twob";
+    // this.executeOne('attack')
+    this.player1.character = "noctis";
+    this.player1.active = this[this[this.turn].character].idle.motion;
+    this.player2.character = "noctis";
     this.player2.active = this[this[this.turn].character].idle.motion;
     this.audio.volume = 0.2;
     this.winsound.volume = 0.2;
     this.audio.play();
-    this.muda.volume = 2;
+    this.muda.volume = 1;
     this.selectionSound.currentTime = 3;
   },
   watch: {
@@ -294,18 +176,16 @@ export default {
   width: 100%;
   height: 100%;
   position: fixed;
-  /* background-image: url("/bg-2.jpeg"); */
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
   background-size: cover;
-  /* object-fit: cover; */
   background-repeat: none;
   transform: scaleX(-1);
 }
 
 .player2 {
-  width: 100vw;
+  width: 100%;
   height: 100%;
   position: fixed;
   -webkit-background-size: cover;
@@ -326,14 +206,12 @@ export default {
   width: 100vw;
   height: 100%;
   position: fixed;
-  /* background-image: url("/bg-2.jpeg"); */
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
   background-size: cover;
   object-fit: cover;
   background-repeat: none;
-  /* transform: scaleX(-1); */
 }
 .box .button {
   margin-left: 5px;
